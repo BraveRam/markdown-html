07aab4f6405ee14a6f44b2ded44ae3a44a40309e
Braveram <lenchomengistu100@gmail.com>
2025-07-11 16:37:17 +0300

    fix placeholder text

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -6,7 +6,7 @@ const Editor = () => {
   return (
     <textarea
       placeholder={
-        mode === "html" ? "Enter markdown here ..." : "Enter HTML here..."
+        mode === "markdown" ? "Enter markdown here ..." : "Enter HTML here..."
       }
       className="w-full min-h-[200px] h-full p-4 border resize-none rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
       aria-label={mode === "html" ? "Markdown input" : "HTML input"}
